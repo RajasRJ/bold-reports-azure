bd9cf6afe4aa568e8492604adc06fac281e97b65
Gunachandar8 <gunachandar.n@syncfusion.com>
2025-09-10 19:10:00 +0530

    458269- Fix the no content issue on get schedule api

--- reporting/viewer/configuration/onpremise/postgresql/versioned_scripts/latest.sql
+++ reporting/viewer/configuration/onpremise/postgresql/versioned_scripts/latest.sql
@@ -1,1 +1,11 @@
 ALTER TABLE BOLDRS_DeploymentReports ADD IsMakeReportPublic smallint NOT NULL;
+
+UPDATE BOLDRS_ScheduleDetail SET IsNoDataEnabled = 0 WHERE IsNoDataEnabled IS NULL;
+
+UPDATE BOLDRS_ScheduleDetail SET EmailContent = 'null' WHERE EmailContent IS NULL;
+
+UPDATE BOLDRS_ScheduleDetail SET Subject = 'null' WHERE Subject IS NULL;
+
+UPDATE BOLDRS_ScheduleDetail SET ScheduleExportInfo = 'null' WHERE ScheduleExportInfo IS NULL;
+
+UPDATE BOLDRS_ScheduleDetail SET ScheduleBucketExportInfo = 'null' WHERE ScheduleBucketExportInfo IS NULL;
